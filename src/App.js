4ae2a30bbcf27d3e0b496dd4aa58c113320228bd
rohan bindu <rbindu42@gmail.com>
2022-06-06 13:27:55 -0700

    payment

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import React, { useEffect } from "react";
 import Header from "./Header";
+import Orders from "./Orders";
 import Home from "./Home";
 import Checkout from "./Checkout";
 import Login from "./Login";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import { auth } from "./firebase";
 import { useStateValue } from "./StateProvider";
+import Footer from "./Footer";
+import Payment from "./Payment";
+import { Elements } from "@stripe/react-stripe-js";
+import { loadStripe } from "@stripe/stripe-js";
+
+const promise = loadStripe(
+  "pk_test_51L7jQlK8GM1tpTQphC6TjLeFuZGAThJIsUNYrQMwIOgSYzN7i8MQGqJMg0oEfzxhOfY7l2xaOzkrBX4T1fzBrPA000Kyz9X4Kc"
+);
 
 function App() {
   const [{}, dispatch] = useStateValue();
@@ -13,7 +22,7 @@ function App() {
     // will only run once when the app component loads...
     // listener
     auth.onAuthStateChanged((authUser) => {
-      console.log("USER IS >>>", authUser);
+      // console.log("USER IS >>>", authUser);
       if (authUser) {
         dispatch({
           type: "SET_USER",
@@ -32,16 +41,30 @@ function App() {
     <Router>
       <div className="app">
         <Switch>
+          <Route path="/orders">
+            <Header />
+            <Orders />
+            <Footer />
+          </Route>
           <Route path="/login">
             <Login />
           </Route>
           <Route path="/checkout">
             <Header />
             <Checkout />
+            <Footer />
+          </Route>
+          <Route path="/payment">
+            <Header />
+            <Elements stripe={promise}>
+              <Payment />
+            </Elements>
+            <Footer />
           </Route>
           <Route path="/">
             <Header />
             <Home />
+            <Footer />
           </Route>
         </Switch>
       </div>
---
+++ src/Footer.js
@@ -0,0 +1,57 @@
+import React from "react";
+import "./Footer.css";
+
+function Footer() {
+  return (
+    <div className="footer">
+      <div className="footer__columns">
+        <h4>Get To Know Us</h4>
+        <h5>Careers</h5>
+        <h5>Blog</h5>
+        <h5>About Amazon</h5>
+        <h5>Sustainability</h5>
+        <h5>Press Center</h5>
+        <h5>Investor Relations</h5>
+        <h5>Amazon Devices</h5>
+        <h5>Amazon Science</h5>
+      </div>
+      <div className="footer__columns">
+        <h4>Make Money with Us</h4>
+        <h5>Sell products on Amazon</h5>
+        <h5>Sell apps on Amazon</h5>
+        <h5>Become an Affiliate</h5>
+        <h5>Become a Delivery Driver</h5>
+        <h5>Start a package delivery business</h5>
+        <h5>Advertise Your Products</h5>
+        <h5>Self-Publish with Us</h5>
+        <h5>Host an Amazon Hub</h5>
+        <h5>> See More Ways to Make Money</h5>
+      </div>
+      <div className="footer__columns">
+        <h4>Amazon Payment Products</h4>
+        <h5>Amazon Rewards Visa Signature Cards</h5>
+        <h5>Amazon Store Card</h5>
+        <h5>Amazon Secured Card</h5>
+        <h5>Amazon Business Card</h5>
+        <h5>Shop with Points</h5>
+        <h5>Credit Card Marketplace</h5>
+        <h5>Reload Your Balance</h5>
+        <h5>Amazon Currency Converter</h5>
+      </div>
+      <div className="footer__columns">
+        <h4>Let Us Help You</h4>
+        <h5>Amazon and Covid-19</h5>
+        <h5>Your Account</h5>
+        <h5>Your Orders</h5>
+        <h5>Shipping Rates & Policies</h5>
+        <h5>Amazon Prime</h5>
+        <h5>Returns & Replacements</h5>
+        <h5>Manage Your Content and Devices</h5>
+        <h5>Amazon Assistant</h5>
+        <h5>Help</h5>
+      </div>
+    </div>
+  );
+}
+
+export default Footer;
