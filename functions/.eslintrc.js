ec90a3291e30a370f3987b1ee5c85ab4140c70f9
rohan bindu <rbindu42@gmail.com>
2022-06-06 18:04:23 -0700

    finished payment functionality

--- functions/.eslintrc.js
+++ functions/.eslintrc.js
@@ -1,13 +1,11 @@
 module.exports = {
+  parser: "@babel/eslint-parser",
   root: true,
   env: {
     es6: true,
     node: true,
   },
-  extends: [
-    "eslint:recommended",
-    "google",
-  ],
+  extends: ["eslint:recommended", "google"],
   rules: {
     quotes: ["error", "double"],
   },
